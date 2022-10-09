2b74420b57458f8e8f2dc53a2626ff868084e853
Rahul Prajapati <prahul3006@gmail.com>
2022-10-09 15:43:51 +0530

    inital commit

---
+++ database/db.js
@@ -0,0 +1,34 @@
+const Sequelize=require('sequelize');
+const db={};
+
+const sequelize=new Sequelize('employee_details','appuser','12345678',{
+    host:'localhost',
+    dialect:'mysql',
+    operatorAliases:0,
+
+    pool:{
+        max:5,
+        min:0,
+        accquire:30000,
+        idle:10000
+    }
+
+})
+
+db.sequelize=sequelize;
+db.Sequelize=Sequelize;
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('DB connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+        process.exit(0);
+    })
+
+
+
+
+module.exports=db;
